d375f517dfb7825dc5346f49d3630970bcff3d8f
mdsaifali09 <saifmr884@gmail.com>
2026-08-13 00:49:20 +0530

    Update production backend URL

--- fronted/src/api/authApi.js
+++ fronted/src/api/authApi.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API =
-  "http://localhost:5000/api/auth";
+  "https://ai-interview-preparation-qg9x.onrender.com/api";
 
 export const registerUser =
   (userData) =>
